2043a3c4b86953b258ad1508a27540b9a31cb38b
Iñaki <icanetealb3f@gmail.com>
2022-07-11 20:25:28 -0300

    Tarea_1

--- meu_utdt_inaki.R
+++ meu_utdt_inaki.R
@@ -1,5 +1,22 @@
+install.packages("tidyverse")
 library(tidyverse)
-install.packages("readr")
+
+install.packages(read)
+
+install.packages("janitor")
+library(janitor)
+
+install.packages("dplyr")
+library(dplyr)
+
 library(readr)
-  read_csv(file = "data/pc_proy21_finfun.csv", stringsAsfactor = TRUE )
-head(pc_proy21_finfun.csv)
+  presu_2021_finfun <- read.csv(file = "data/pc_proy21_finfun.csv" ,
+                                encoding = "UTF-8")
+presu_2021_finfun_filtrado <- filter(presu_2021_finfun , 
+                                     Descripción.Jurisdicción == "Ministerio de Educación")
+  
+presu_2021_finfun_select <- select(presu_2021_finfun, -Descripción.Entidad, -Código.Entidad)
+
+presu_2021_finfun_mutado <- mutate(presu_2021_finfun_filtrado, 
+                                   Jurisdicción.FinFun=paste
+                                   (Descripción.Jurisdicción,Descripción.Finalidad.Función,sep="_"))
